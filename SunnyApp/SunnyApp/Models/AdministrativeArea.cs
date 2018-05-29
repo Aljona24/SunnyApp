234bdf76129128347e0ea80227cc3a70ee5feb50
alona.barabash <alona.barabash@mail.ru>
2018-05-29 18:00:57 +0300

    class AdministrativeArea has been added

---
+++ SunnyApp/SunnyApp/Models/AdministrativeArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunnyApp.Models
+{
+    public class AdministrativeArea
+    {
+        public string LocalizedName { get; set; }
+        public string LocalizedType { get; set; }
+    }
+}
--- SunnyApp/SunnyApp/Models/Location.cs
+++ SunnyApp/SunnyApp/Models/Location.cs
@@ -8,5 +8,6 @@ namespace SunnyApp.Models
         public string LocalizedName { get; set; } 
         public string Type { get; set; } 
         public Country Country { get; set; }
+        public AdministrativeArea AdministrativeArea { get; set; }
     }
 }
