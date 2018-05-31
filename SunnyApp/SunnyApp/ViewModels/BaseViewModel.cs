c806a994f3575b7c5650558c2195c52a3cfe2f20
alona.barabash <alona.barabash@mail.ru>
2018-05-31 17:30:19 +0300

    Renamed SetProperty -> SetPropertyIfDiff

--- SunnyApp/SunnyApp/ViewModels/BaseViewModel.cs
+++ SunnyApp/SunnyApp/ViewModels/BaseViewModel.cs
@@ -16,17 +16,17 @@ namespace SunnyApp.ViewModels
         public bool IsBusy
         {
             get { return isBusy; }
-            set { SetProperty(ref isBusy, value); }
+            set { SetPropertyIfDiff(ref isBusy, value); }
         }
 
         string title = string.Empty;
         public string Title
         {
             get { return title; }
-            set { SetProperty(ref title, value); }
+            set { SetPropertyIfDiff(ref title, value); }
         }
 
-        protected bool SetProperty<T>(ref T backingStore, T value,
+        protected bool SetPropertyIfDiff<T>(ref T backingStore, T value,
             [CallerMemberName]string propertyName = "",
             Action onChanged = null)
         {
@@ -44,10 +44,8 @@ namespace SunnyApp.ViewModels
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             var changed = PropertyChanged;
-            if (changed == null)
-                return;
 
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            changed?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
     }
